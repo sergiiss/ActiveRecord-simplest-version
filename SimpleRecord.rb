08328d790c40cfca8657341b8359ccc8024511b3
Sergiiss <Dracula23@mail.ru>
2017-06-05 20:41:25 +0300

    Add method order

--- SimpleRecord.rb
+++ SimpleRecord.rb
@@ -1,6 +1,31 @@
 class SimpleRecord
   def self.has_many(symbol)
-    define_method(symbol) { puts "SELECT * FROM #{symbol}" }
+    define_method(symbol) do
+      SimpleScope.new(symbol)
+    end
+  end
+end
+
+class SimpleScope
+  attr_reader :symbol, :foo
+
+  def initialize(symbol)
+    @symbol = symbol
+  end
+
+  def to_s
+    puts "SELECT * \nFROM #{symbol} \n#{foo}"
+  end
+
+  def order(column_name)
+    @foo =
+      case column_name
+      when "created_at DESC"
+        "ORDER BY #{column_name}"
+      when "created_at ASC" || "created_at"
+        "ORDER BY #{column_name}"
+      end
+    self
   end
 end
 
@@ -9,11 +34,4 @@ class Post < SimpleRecord
 end
 
 p = Post.new
-p.comments
-
-
-
-
-
-
-
+p.comments.order("created_at ASC").to_s
