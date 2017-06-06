2d6aee447e01eb1b69e5ad1479ad525e21c141b8
Sergiiss <Dracula23@mail.ru>
2017-06-07 01:27:05 +0300

    Add tests, change the logic of the method order

--- lib/simple_record/simple_scope.rb
+++ lib/simple_record/simple_scope.rb
@@ -1,22 +1,23 @@
 module SimpleRecord
   class SimpleScope
-    attr_reader :symbol, :foo
+    attr_reader :symbol, :order_sql
 
     def initialize(symbol)
       @symbol = symbol
     end
 
     def to_s
-      "SELECT * \nFROM #{symbol} \n#{foo}"
+      "SELECT * \nFROM #{symbol}#{order_sql}"
     end
 
     def order(column_name)
-      @foo =
-        case column_name
-        when "created_at DESC"
-          "ORDER BY #{column_name}"
-        when "created_at ASC" || "created_at"
-          "ORDER BY #{column_name}"
+      column_name =~ /^.*\s(asc|ASC|desc|DESC)$/
+
+      @order_sql =
+        if $1
+          "\nORDER BY #{column_name}"
+        else
+          "\nORDER BY #{column_name} ASC"
         end
       self
     end
--- test/simple_record_test.rb
+++ test/simple_record_test.rb
@@ -1,17 +1,31 @@
 require 'test_helper'
 
 class TestSimpleRecord < Minitest::Test
-  def test_order
-    p = Post.new
-    result = p.comments.order("created_at ASC").to_s
+  def test_order_ASC
+    post = Post.new
+    result_sql = post.comments.order("created_at ASC").to_s
 
-    assert_equal "SELECT * \nFROM comments \nORDER BY created_at ASC", result
+    assert_equal "SELECT * \nFROM comments\nORDER BY created_at ASC", result_sql
+  end
+
+  def test_order_desc
+    post = Post.new
+    result_sql = post.comments.order("created_at desc").to_s
+
+    assert_equal "SELECT * \nFROM comments\nORDER BY created_at desc", result_sql
+  end
+
+  def test_order
+    post = Post.new
+    result_sql = post.comments.order("created_at").to_s
+
+    assert_equal "SELECT * \nFROM comments\nORDER BY created_at ASC", result_sql
   end
 
   def test_method_through_symbol
-    p = Post.new
-    result = p.comments.to_s
+    post = Post.new
+    result_sql = post.comments.to_s
 
-    assert_equal "SELECT * \nFROM comments \n", result
+    assert_equal "SELECT * \nFROM comments", result_sql
   end
 end
